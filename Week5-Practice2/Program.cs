bf9fa33ea9b804de2cd1bc612a1dc98725ec028a
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-08-15 10:57:38 +0300

    Add project files.

---
+++ Week5-Practice2/Program.cs
@@ -0,0 +1,16 @@
+﻿List<string> davetliler = new();
+davetliler.Add("Polat Alemdar");
+davetliler.Add("Süleyman Çakır");
+davetliler.Add("Laz Ziya");
+davetliler.Add("Testere Necmi");
+davetliler.Add("Karahanlı");
+davetliler.Add("İplikçi nedim");
+davetliler.Add("Memati Baş");
+
+
+
+Console.WriteLine("**Davetliler**");
+for (int i = 0; i < davetliler.Count; i++)
+{
+    Console.WriteLine($"{i+1} - {davetliler[i]}");
+}
